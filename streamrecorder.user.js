147cd7a2f2e074d0899ab40c7bfbe82daefc18ef
Ladroop <ladroop@hotmail.com>
2026-02-26 23:21:28 +0100

    Update streamrecorder.user.js
    complete re-work

--- streamrecorder.user.js
+++ streamrecorder.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Stream recorder for adult cam sites that do not support HLS
 // @namespace    Everywhere
-// @version      1.0.9
+// @version      1.1.0
 // @description  Record stripchat, livejasmin and other cam sites that do not full support HLS/m3u8
 // @author       Ladroop
 // @license	     MIT
-// @copyright    2025 Ladroop
+// @copyright    2026 Ladroop
 // @match        https://*.stripchat.com/*
 // @match        https://www.livejasmin.com/*
 // @match        https://www.xcams.com/*
@@ -16,9 +16,9 @@
 // @match        https://showup.tv/*
 // @noframes
 // @run-at       document-end
-// @grant        none
-// @downloadURL https://update.sleazyfork.org/scripts/508891/Stream%20recorder%20for%20adult%20cam%20sites%20that%20do%20not%20support%20HLS.user.js
-// @updateURL https://update.sleazyfork.org/scripts/508891/Stream%20recorder%20for%20adult%20cam%20sites%20that%20do%20not%20support%20HLS.meta.js
+// @grant        GM_download
+// @downloadURL https://openuserjs.org/install/ladroop2/Stream_recorder_for_adult_cam_sites_that_do_not_support_HLS.user.js
+// @updateURL https://openuserjs.org/meta/ladroop2/Stream_recorder_for_adult_cam_sites_that_do_not_support_HLS.meta.js
 // ==/UserScript==
 
 
@@ -65,41 +65,59 @@
         '</g></g></svg>';
 
     var status="stop";
-    var count=0;
     var recName="";
-    var location=document.location.href;
-    var site=location.split("/")[2].split(".");
+    var site=document.location.href.split("/")[2].split(".");
     site=site[site.length-2];
-
+    var location="";
     var mediaRecorder;
-    var recordedBlobs;
+    var recordedBlobs=[];
     var stream;
     var video;
     var blob;
     var vidObj=0;
-    var observer= new MutationObserver(pagechange1);
-    var wtime=false;
-    var interval=false;
     var firefox=false;
-	var starttime="";
+    var starttime="";
+    var rectime=0;
+    var stopPressed=false;
+    var mediarecorderInit=false;
+    var timeoutID=0;
+    var timeoutID2=0;
+    var timeoutID3=0;
+    var savetime=20;
+    var mimeType=['video/mp4',
+                  'video/webm',
+                  'video/webm'];
+    var extention=['.mp4',
+                   '.webm',
+                   '.webm'];
+    var container=['video/mp4',
+                   'video/webm',
+                   'video/webm'];
+    var n=0;
+    for(n=0;n < mimeType.length-1;n++) {
+        if (MediaRecorder.isTypeSupported(mimeType[n])){break;}
+    }
+    var type="";
 
-
-    pagechange();
+    setInterval(pagechange1,1000);
 
     function pagechange1(){
-        if (wtime){return;}
-        wtime=true;
-        setTimeout(function(){
-            if (document.location.href==location){wtime=false;return;}
-            observer.disconnect();
-            wtime=false;
-            if (status=="rec"){stoprecord();}
-            location=document.location.href;
-            pagechange();
-       },100);
+        if (document.location.href==location){return;}
+        location=document.location.href;
+        clearTimeout(timeoutID2);
+        clearTimeout(timeoutID3);
+        stopPressed=true;
+        if ((status=="rec")||(status=="pauze")){
+            stoprecord();
+        }
+        pagechange();
     }
 
     function pagechange(){
+        if (status != "stop"){
+            setTimeout(pagechange,1000);
+            return;
+        }
         if (document.getElementById("popitup1")){
             document.getElementById("popitup1").remove();
         }
@@ -108,112 +126,133 @@
 
     function getrecname(){
         var name="video";
+
         if (site=="livejasmin"){
-            observer.observe(document.body,{attributes: true});
             if (location.indexOf("chat")==-1){return;}
             name=location.split("/")[location.split("/").length-1];
             vidObj=1;
         }
+
         if (site=="stripchat"){
             name=location.split("/")[3].split("?")[0];
-            observer.observe(document.head,{subtree: true, characterData: true, childList: true });
         }
+
         if (site=="showup"){
-            observer.observe(document.head,{subtree: true, characterData: true, childList: true });
             name=location.split("/")[3];
             if(name=="en"){
                 name=location.split("/")[4];
             }
             if(!name){return;}
         }
+
         if (site=="secretfriends"){
             name=location.split("/")[4].split("?")[0];
         }
+
         if (site=="xcams"){
-            observer.observe(document.body,{attributes: true});
             if (location.split("/")[4]!="chat"){return;}
             name=location.split("/")[5];
         }
+
         if (site=="manyvids"){
-            if(!interval){
-                setInterval(pagechange1,2000)
-                interval=true;
-            }
             if (location.split("/")[4]!="cam"){return;}
             name=location.split("/")[5];
         }
+
         if (site=="soulcams"){
             name=location.split("/")[4];
-            observer.observe(document.head,{subtree: true, characterData: true, childList: true });
         }
+
         if (site=="cameraprive"){
             name=location.split("/")[5];
         }
+
         recName=site+"-"+name;
-        wtime=true;
-        setTimeout(findvideo,6000);
+
+        timeoutID2=setTimeout(findvideo,6000);
     }
 
     function findvideo(){
-        wtime=false;
         if (document.getElementsByTagName("video").length > vidObj){
             video=document.getElementsByTagName("video")[vidObj];
+            if (video.srcObject==null){
+                if (video.src.indexOf("blob:")==-1){return;}
+            }
             getStream();
         }
     }
 
     function getStream(){
         stream = video.captureStream ? video.captureStream() : video.mozCaptureStream();
-        if (!video.captureStream) {firefox=true}
+        if (!video.captureStream){firefox=true;}
         makepopitup();
         document.getElementById("minivideo").srcObject=stream;
     }
 
-    function startDownload() {
-        blob = new Blob(recordedBlobs, {type: 'video/webm'});
-        var url = window.URL.createObjectURL(blob);
-        var a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        a.download = recName+starttime+'.webm';
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function(){
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-            recordedBlobs = [];
-        }, 100);
+    function autosave(){
+        if (rectime<10){
+            dlready();
+            return;
+        }
+        status="saving";
+        msg("Saving");
+        blob = new Blob(recordedBlobs, {type: container[n]});
+        GM_download({
+            url: blob,
+            name: recName+starttime+extention[n],
+            onload: dlready,
+        });
     }
 
-    function handleDataAvailable(event) {
-        msg("Recording "+count);
-        count++;
-        if (event.data && event.data.size > 0) {
-            recordedBlobs.push(event.data);
+    function dlready(){
+        blob="";
+        recordedBlobs=[];
+        status="stop";
+        if((document.getElementById("autosave").value==1)&&(stopPressed==false)){
+            if(rectime<savetime*60){
+                msg("Waiting");
+                setTimeout(startRecording,5000);
+                return;
+            }
+            startRecording();
+            return;
         }
+        msg("Stopped");
+        document.getElementById("recbutton").style.display="inline-block";
+        document.getElementById("pauzebutton").style.display="none";
+        document.getElementById("pauzerecbutton").style.display="none";
     }
 
     function startRecording() {
-		starttime="_"+new Date().toISOString().split(".")[0]+"GMT";
-        starttime=starttime.replaceAll(":","-");
-        count=0;
-        recordedBlobs = [];
-        try {
-            mediaRecorder = new MediaRecorder(stream);
-        } catch (e) {
-            msg("Not supported");
-            status="stop";
-            document.getElementById("recbutton").style.display="inline-block";
-            document.getElementById("pauzebutton").style.display="none";
-            document.getElementById("pauzerecbutton").style.display="none";
-            setTimeout(function(){msg("Stopped");},3000);
-            return;
+        if (mediarecorderInit){recordStart();return;}
+        var bitRate=video.videoHeight*video.videoWidth*3;
+        if (bitRate> 8000000){bitRate=8000000;}
+        type=mimeType[n];
+        if (document.getElementById("avc3").value==1){
+            type='video/mp4; codecs="avc3.64001F, mp4a.40.2"';
         }
+        mediaRecorder = new MediaRecorder(stream,{
+            mimeType:type,
+            VideobitsPerSecond: bitRate,
+            AudiobitsPerSecond: 64000
+        });
         mediaRecorder.onstop = mediaRecorderStopped;
         mediaRecorder.ondataavailable = handleDataAvailable;
+        mediarecorderInit=true;
+        recordStart();
+    }
+
+    function recordStart(){
+        starttime="_"+new Date().toISOString().split(".")[0]+"GMT";
+        starttime=starttime.replaceAll(":","_");
+        rectime=0;
+        stopPressed=false;
+        status="rec";
+        document.getElementById("recbutton").style.display="none";
+        document.getElementById("pauzebutton").style.display="inline-block";
         try {
             mediaRecorder.start(1000);
-        } catch (e) {console.log(e);
+        } catch (e) {
             msg("Error");
             status="stop";
             document.getElementById("recbutton").style.display="inline-block";
@@ -222,41 +261,41 @@
             setTimeout(function(){msg("Stopped");},3000);
             return;
         }
+        clearTimeout(timeoutID);
+        timedisplay();
+    }
+
+    function handleDataAvailable(event) {
+        clearTimeout(timeoutID3);
+        if (event.data && event.data.size > 0) {
+            recordedBlobs.push(event.data);
+        }
+        timeoutID3=setTimeout(stoprecord,100000);
     }
 
     function mediaRecorderStopped(){
-        msg("Stopped");
+        clearTimeout(timeoutID3);
         document.getElementById("recbutton").style.display="inline-block";
         document.getElementById("pauzebutton").style.display="none";
         document.getElementById("pauzerecbutton").style.display="none";
-        status="stop";
-        startDownload();
-    }
-
-    function stopRecording() {
-        msg("Stopped");
-        mediaRecorder.stop();
-    }
-
-    function msg(message){
-        document.getElementById("message123").innerHTML=message;
+        autosave();
     }
 
     function stoprecord(){
         if (status=="stop"){return;}
-        status="stop";
-        stopRecording();
+        if (status=="saving"){return;}
+        stopPressed=true;
+        mediaRecorder.stop();
     }
 
     function startrecord(){
         if (status!="stop"){return;}
-        status="rec";
-        document.getElementById("recbutton").style.display="none";
-        document.getElementById("pauzebutton").style.display="inline-block";
+        mediarecorderInit=false;
         startRecording();
     }
 
     function pauzerecord(){
+        status="pauze";
         mediaRecorder.pause();
         document.getElementById("pauzebutton").style.display="none";
         document.getElementById("pauzerecbutton").style.display="inline-block";
@@ -269,38 +308,79 @@
         document.getElementById("pauzerecbutton").style.display="none";
     }
 
+    function timedisplay(){
+        if (status=="rec"){
+            if ((document.getElementById("autosave").value==1)&&(rectime>savetime*60)){
+                mediaRecorder.stop();
+                return;
+            }
+            var h = Math.floor(rectime / 3600);
+            var H=("0"+h).substr(-2);
+            var m = Math.floor(rectime % 3600 / 60);
+            var M=("0"+m).substr(-2);
+            var s = Math.floor(rectime % 3600 % 60);
+            var S=("0"+s).substr(-2);
+            msg("Rec.&nbsp"+H+"h&nbsp"+M+"m&nbsp"+S+"s");
+            rectime++;
+        }
+        timeoutID=setTimeout(function(){
+            if (status != "stop"){
+                timedisplay();
+            }
+        },1000);
+    }
+
     function miniunmute(){
         document.getElementById("minivideo").muted=false;
         document.getElementById("minimute").style.display="none";
     }
+    function msg(message){
+        document.getElementById("message123").innerHTML=message;
+    }
 
     function makepopitup(){
-        var popstyle="color:black;z-index:100000;top:100px;left:10px;box-shadow:0px 0px 32px rgba(0, 0, 0, 0.32);border-radius:4px;border:1px solid rgb(221, 221, 221);background-color:rgb(200, 200, 200);position:fixed; display:inline-block; height: auto; width:auto; padding: 6px 0px 6px 6px;";
+        var popstyle="font-family: monospace;font-size: 12px;color:black;z-index:100000;top:100px;left:10px;box-shadow:0px 0px 32px rgba(0, 0, 0, 0.32);border-radius:4px;border:1px solid rgb(221, 221, 221);background-color:rgb(200, 200, 200);position:fixed; display:inline-block; height: auto; width:auto; padding: 6px 6px 6px 6px;";
         var newelem=document.createElement('span');
         newelem.setAttribute("style", popstyle);
         newelem.id="popitup1";
+        newelem.className="notranslate";
         var newdiv=document.createElement('div');
         newdiv.style.width="100%";
         newdiv.style.height="50px";
-        newdiv.innerHTML=pauzebutton+pauzerecbutton+recbutton+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+stopbutton;
+        newdiv.innerHTML="<span>"+pauzebutton+pauzerecbutton+recbutton+"</span><span style='float:right'>"+stopbutton+"</span>";
         newelem.appendChild(newdiv);
+        newdiv=document.createElement('div');
+        newdiv.style.position="relative";
+        newdiv.style.width="100%";
+        newdiv.style.height="20px";
+         newdiv.innerHTML="<span>Autosave:</span><span style='float:right;position: absolute; right: 5px; top:1px;'><input type='range' id='autosave' min=0 max=1 value=0 style='width: 40px;height:11px;cursor: pointer;zoom:0.8'></span>";
+        newelem.appendChild(newdiv);
+        newdiv=document.createElement('div');
+        newdiv.style.position="relative";
+        newdiv.style.width="100%";
+        newdiv.style.height="20px";
+        newdiv.innerHTML="<span>Use avc3:</span><span style='float:right;position: absolute; right: 5px; top:1px;'><input type='range' id='avc3' min=0 max=1 value=0 style='width: 40px;height:11px;cursor: pointer;zoom:0.8'></span>";
+        newdiv.style.display="none";
+        if (MediaRecorder.isTypeSupported('video/mp4; codecs="avc3.64001F, mp4a.40.2"')){
+          newdiv.style.display="block";
+        }
+        newelem.appendChild(newdiv);
+        var newdiv2=document.createElement('div');
         newdiv=document.createElement('div');
         newdiv.style.width="100%";
         newdiv.style.height="20px";
         newdiv.style.color="black";
         newdiv.id="message123";
-        newdiv.className="notranslate";
         newdiv.innerHTML="Stopped";
         newdiv.style.cursor="move";
         newdiv.addEventListener("mousedown",dragMouseDown);
         newelem.appendChild(newdiv);
-        var newdiv2=document.createElement('div');
         newdiv2.style.position="relative";
         if (firefox){
             newdiv=document.createElement('div');
             newdiv.innerHTML="🔇";
             newdiv.id="minimute";
-            newdiv.style.position="absolute"
+            newdiv.style.position="absolute";
             newdiv.style.zIndex="100001";
             newdiv.style.top="0px";
             newdiv.style.cursor="pointer";
@@ -313,6 +393,7 @@
         newdiv.style.height="auto";
         newdiv.style.border="1px solid rgb(20, 20, 20)";
         newdiv.style.top="0px";
+        newdiv.style.margin="2px";
         newdiv.style.backgroundColor="black";
         newdiv.id="minivideo";
         newdiv.muted = true;
